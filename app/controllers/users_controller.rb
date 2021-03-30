2fce0c8b73ee5092dfa9a71489b6f64023172f92
Sabine Stuck <sabine.moreira95@gmail.com>
2021-03-30 12:55:51 +0200

    changed the home page, the controller to save the users and routes to be able to see users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ require 'json'
 require 'open-uri'
 
 class UsersController < ApplicationController
+  skip_forgery_protection
+  
     def index
         url = "https://randomuser.me/api/"
         user_serialized = open(url).read
@@ -10,21 +12,28 @@ class UsersController < ApplicationController
 
     def show
         if params[:number]
+            @number = params[:number]
             url = "https://randomuser.me/api/?results=#{params[:number]}"
             user_serialized = open(url).read
             @users = JSON.parse(user_serialized)
         end
+        if params[:users]
+          @users = params[:users]
+        end
     end
-    
+
     def create
-        @user = User.new(user_params)
-    
+        @users = params[:users]
+
+        @user = User.new({ :title => params[:title], :first_name => params[:first_name],  :last_name => params[:last_name], :address => params[:address], :phone => params[:phone], :email => params[:email]})
+
         if @user.save
-          redirect_to @user
+          redirect_to action: "show", users: JSON.encode(@users)
         else
           render :new
         end
-      end
+
+    end
 
     private
       def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/users", to: "users#index"
   post "/users", to: "users#show"
+  post "/create", to: "users#create"
 end
